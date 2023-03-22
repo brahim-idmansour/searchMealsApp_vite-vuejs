21c4b7aaceee1aa696ddafc990196ef06fc11c5a
brahim idmansour <brahimidmansour24@gmail.com>
2023-03-22 17:03:09 +0100

    create store files

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from "vuex";
-import actions from "./actions";
-import mutations from "./mutations";
-import getters from "./getters";
+import * as actions from "./actions";
+import * as mutations from "./mutations";
+import * as getters from "./getters";
 import state from "./state";
 
 const store = createStore({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>home</div>
+  <pre>{{ meals }}</pre>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+
+const meals = computed(() => store.state.meals);
+</script>
