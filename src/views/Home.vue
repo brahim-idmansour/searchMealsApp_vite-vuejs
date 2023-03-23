b71beb35ea07805cd5b1f9941209d9f3dcb98246
brahim idmansour <brahimidmansour24@gmail.com>
2023-03-23 14:45:30 +0100

    creat mealList vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,29 @@
 <template>
-  <input type="text" />
+  <div class="flex flex-col p-8">
+    <div class="items-center justify-center">
+      <input
+        type="text"
+        class="rounded border-2 border-gray-200 w-full"
+        placeholder="search for meals"
+      />
+    </div>
+    <div class="flex justify-center gap-2 mt-2">
+      <!-- <pre>{{ letters }}</pre> -->
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        :key="letter"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import { computed } from "vue";
 import store from "../store";
 
-const meals = computed(() => store.state.meals);
+// const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTVWXYZ".split("");
 </script>
