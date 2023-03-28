62a1904ccaae7f30c958289ee237af1753579025
brahim idmansour <brahimidmansour24@gmail.com>
2023-03-28 12:44:50 +0000

    mealsByName && mealsDetails

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-8">
+  <div class="p-8 pb-0">
     <input
       type="text"
       v-model="keyword"
@@ -9,20 +9,63 @@
     />
   </div>
 
-  <div class="">
-    <pre>{{ meals }}</pre>
+  <div class="grid grid-cols-1 md:grid-cols-4 gap-5 p-8">
+    <!-- <pre>{{ meals }}</pre> -->
+    <div
+      v-for="meal of meals"
+      :key="meal.idMeal"
+      class="bg-white shadow rounded-xl"
+    >
+      <router-link :to="{ name: 'mealDetails', params: { id: meal.idMeal } }">
+        <img
+          :src="meal.strMealThumb"
+          :alt="meal.strMeal"
+          class="rounded-t-xl w-full h-48 object-cover"
+        />
+      </router-link>
+      <div class="p-3">
+        <h3 class="font-bold">{{ meal.strMeal }}</h3>
+        <p class="mb-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero ab
+          fuga maxime velit explicabo iste alias, quod a ipsa, qui, distinctio
+          aut tempora nemo voluptatum hic quos corrupti perferendis?
+        </p>
+        <div class="">
+          <a
+            :href="meal.strYoutube"
+            target="_blank"
+            class="px-3 py-2 rounded border-2 text-white border-red-600 bg-red-500 hover:bg-red-600 transition-colors"
+            >YouTube</a
+          >
+          <!-- <router-link
+            to="/"
+            class="px-3 py-2 rounded border-2 text-white border-red-600 bg-red-500 hover:bg-red-600 transition-colors"
+          >
+            View
+          </router-link> -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
 import { computed } from "@vue/reactivity";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
 import store from "../store";
 
+const route = useRoute();
 const keyword = ref("");
 const meals = computed(() => store.state.searchedMeals);
 
 function searchMeals() {
   store.dispatch("searchMeals", keyword.value);
 }
+onMounted(() => {
+  keyword.value = route.params.name;
+  if (keyword.value) {
+    searchMeals();
+  }
+});
 </script>
